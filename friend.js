14ef45eaff82cf6caab3c04c5b9ede453957b6cf
nadim <nadimhasannayan77@gmail.com>
2024-01-28 23:27:16 +0600

    friend

---
+++ friend.js
@@ -0,0 +1,20 @@
+score = 30;
+frndScore = 30;
+if(score >=80){
+    // frndScore = 80;
+    if(frndScore >= 80){
+        console.log('lets go for lunch')
+    }
+    else if(frndScore >= 60){
+        console.log('good luck next time')
+    }
+    else if(frndScore >= 40){
+        console.log('unseen friend message')
+    }
+    else if(frndScore < 40){
+        console.log('block your friend')
+    }
+}
+else{
+    console.log('go to home')
+}
